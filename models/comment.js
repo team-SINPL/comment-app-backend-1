a0f8873e2f7df940a3940fc5c50a22f74e1896db
Ishan Avishka <49125943+I3han@users.noreply.github.com>
2021-10-02 00:51:44 +0530

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -4,16 +4,13 @@ const DataTypes = require('sequelize/lib/data-types');
 
 const Comment = db.define('comment', {
     id: {
-        
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-    
     content: DataTypes.STRING,
     type: DataTypes.STRING,
     agreeCount: DataTypes.INTEGER,
-    
     disagreeCount: DataTypes.INTEGER,
     postId: DataTypes.INTEGER
 });
